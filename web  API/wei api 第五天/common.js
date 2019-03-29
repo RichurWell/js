ea769fd6684676664cca569ec15319aeba806e15
liuwei <1397861069@qq.com>
2019-03-29 18:57:09 +0800

    web api 结束

---
+++ web  API/wei api 第五天/common.js
@@ -0,0 +1,21 @@
+function my$(id) {
+    return document.getElementById(id);
+}
+
+//设置任意元素的中间文本内容
+function setInnerText(element, text) {
+    if (typeof element.textContent == "undefined") {
+        element.innerText = text;
+    } else {
+        element.textContent = text;
+    }
+}
+
+//获取任意元素的中间文本内容
+function getInnerText(element) {
+    if (typeof element.textContent == "undefined") {
+        return element.innerText;
+    } else {
+        return element.textContent;
+    }
+}
